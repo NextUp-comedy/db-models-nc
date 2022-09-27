aab37d945b99e2835476a5aa3bc53126afef8612
Ibarhim-Jarada <ibrahim.jarada@gmail.com>
2022-09-27 12:38:59 +0300

    remove migrations folder

--- src/database/migrations/migrateDown.ts
+++
@@ -1,7 +0,0 @@
-import { sequelize } from 'db-models-nc';
-import umzug from '../../../migration';
-
-(async (): Promise<void> => {
-  await umzug.down();
-  sequelize.close();
-})();
--- src/database/migrations/migrateUp.ts
+++
@@ -1,7 +0,0 @@
-import { sequelize } from '../..';
-import umzug from '../../index';
-
-(async (): Promise<void> => {
-  await umzug.up();
-  sequelize.close();
-})();
