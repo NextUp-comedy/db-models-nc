44bd5031adb4593f67cabd08d1d8d856ffda0141
zaher-aa <zaherabuamro@gmail.com>
2022-09-28 12:08:20 +0300

    chore: used Promise.all instead of too many async calls

--- src/migrations/20220804110541-insert-into-users-table.ts
+++ src/migrations/20220804110541-insert-into-users-table.ts
@@ -2,31 +2,61 @@ import { users } from '..';
 import { Migration } from '../migration';
 
 const up: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.removeConstraint('users', 'users_created_by_fkey');
-  await queryInterface.removeConstraint('users', 'users_updated_by_fkey');
+  await Promise.all([
+    queryInterface.removeConstraint('users', 'users_created_by_fkey'),
+    queryInterface.removeConstraint('users', 'users_updated_by_fkey'),
+    queryInterface.removeConstraint('users', 'users_user_role_id_fkey'),
+    queryInterface.removeConstraint('users', 'users_user_status_id_fkey'),
+  ]);
+
   await queryInterface.bulkInsert('users', users);
-  await queryInterface.addConstraint('users', {
-    fields: ['created_by'],
-    type: 'foreign key',
-    name: 'users_created_by_fkey',
-    references: { // Required field
-      table: 'users',
-      field: 'id',
-    },
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-  });
-  await queryInterface.addConstraint('users', {
-    fields: ['updated_by'],
-    type: 'foreign key',
-    name: 'users_updated_by_fkey',
-    references: { // Required field
-      table: 'users',
-      field: 'id',
-    },
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-  });
+
+  await Promise.all([
+    queryInterface.addConstraint('users', {
+      fields: ['created_by'],
+      type: 'foreign key',
+      name: 'users_created_by_fkey',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    }),
+    queryInterface.addConstraint('users', {
+      fields: ['updated_by'],
+      type: 'foreign key',
+      name: 'users_updated_by_fkey',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    }),
+    queryInterface.addConstraint('users', {
+      fields: ['user_role_id'],
+      type: 'foreign key',
+      name: 'users_user_role_id_fkey',
+      references: {
+        table: 'user_roles',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    }),
+    queryInterface.addConstraint('users', {
+      fields: ['user_status_id'],
+      type: 'foreign key',
+      name: 'users_user_status_id_fkey',
+      references: {
+        table: 'user_statuses',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    }),
+  ]);
 };
 
 const down: Migration = async ({ context: queryInterface }) => {
